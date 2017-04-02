d6e406fce05a74d1dcff7878182f40c66fcc3c3d
Alessandro Costa <alecosta.me@gmail.com>
2017-04-02 16:37:37 +0200

    First version of 'stations' api

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+import collections
+import json
+import requests
 
 app = Flask(__name__)
+
+# ----- APIs --------------------------------------------------------------------------------------
+
+@app.route("/api/v1/stations/<txt>")
+def stations_lookup(txt=None):
+    r = requests.get("http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/%s" % (txt))
+    unordered_dict = { line.split("|")[0] : line.split("|")[1] for line in r.text.splitlines() if len(line) > 0 }
+    output_dict = collections.OrderedDict(sorted(unordered_dict.items())) if len(unordered_dict) > 0 else {}
+    #return json.dumps(output_dict, separators=(',',':'))
+    return jsonify(output_dict)
+
+# ----- Main routes -------------------------------------------------------------------------------
 
 @app.route("/")
 def index():
@@ -9,6 +24,8 @@ def index():
 @app.route("/starter")
 def starter():
     return render_template('starter.html')
+
+# ----- Main function -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
     app.run()
--- static/js/init.js
+++ static/js/init.js
@@ -1,7 +1,50 @@
-(function($){
-  $(function(){
+var changeTimer = false;
 
+/*
+(function($) {
+    $(function() {
+        
+
+        $("#station").on("change",function() {
+            if(changeTimer !== false) clearTimeout(changeTimer);
+            changeTimer = setTimeout( function() {
+                console.log("timerz")
+                changeTimer = false;
+            }, 300);
+        });
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+*/
+
+function setStations(stations) {
+    console.log("setStations");
+    console.log(stations);
+    number_of_stations = Object.keys(stations).length
+    console.log(number_of_stations)
+    if (number_of_stations > 0) {
+        $("#stationmsg").html(number_of_stations + " STATIONS FOUND");
+    } else {
+        $("#stationmsg").html("NO STATIONS");
+    }
+}
+
+$(document).ready( function() {
     $('.button-collapse').sideNav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    $("#station").on('input', function() {
+        if(changeTimer !== false) clearTimeout(changeTimer);
+        changeTimer = setTimeout( function() {
+            console.log("search stations")
+            if ( $("#station").val().length > 0 ) {
+                $.get( "/api/v1/stations/" + $("#station").val(), function(data) {
+                    //console.log("get finished")
+                    //console.log(data)
+                    //$( "body" ).append(data)
+                    setStations(data);
+                }, "json" );
+            } else {
+                $("#stationmsg").html("");
+            }
+        }, 300);
+    });
+});
