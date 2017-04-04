76448df380bafbedfa9e176e1abba4895b0c23e1
Alessandro Costa <alecosta.me@gmail.com>
2017-04-04 08:00:48 +0200

    Found stations shown in a responsive grid

--- src/static/js/init.js
+++ src/static/js/init.js
@@ -5,16 +5,43 @@ function setStations(stations) {
     console.log(stations);
     number_of_stations = Object.keys(stations).length
     console.log(number_of_stations)
-    if (number_of_stations > 0) {
-        $("#stationmsg").html(number_of_stations + " STATIONS FOUND");
+    
+    if (number_of_stations == 1) {
+        $("#stations_msg").html("<div class=\"chip\">" + number_of_stations + " station found</div>");
+    } else if (number_of_stations > 0) {
+        $("#stations_msg").html("<div class=\"chip\">" + number_of_stations + " stations found</div>");
     } else {
-        $("#stationmsg").html("NO STATIONS");
+        $("#stations_msg").html("<div class=\"chip\">no stations</div>");
     }
+
+    $("#stations_container").html("");
+    var tot_counter = 1;
+    var counter = 0;
+    var htmlrow = "";
+    Object.keys(stations).forEach( function(key) {
+        console.log("Processing station: " + key + " " + stations[key]);
+        if (counter == 0) {
+            htmlrow += "<div class=\"row\">";
+        }
+        htmlrow += "<div class=\"col s12 m3\"><div class=\"card-panel red darken-2\"><span class=\"white-text\">"
+        htmlrow += key;
+        htmlrow += "</span></div></div>"
+        if (counter == 3 || tot_counter == number_of_stations) {
+            htmlrow += "</div>\n";
+            $("#stations_container").append(htmlrow);
+            htmlrow = "";
+            counter = 0;
+        } else {
+            counter++;
+        }
+        tot_counter++;
+    });
 }
 
 $(document).ready( function() {
-    // navbar
+    // initializations
     $('.button-collapse').sideNav();
+    $('.chips').material_chip();
 
     // stations websocket
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/stationsws');
@@ -45,7 +72,8 @@ $(document).ready( function() {
                 }, "json" );*/
             } else {
                 console.log("empty input")
-                $("#stationmsg").html("");
+                $("#stations_msg").html("");
+                $("#stations_container").html("");
             }
         }, 300);
     });
